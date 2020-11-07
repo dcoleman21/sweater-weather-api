1fbb29ae2901495f31f56d589b2c0bba13affc03
Dani Coleman <danicoleman00@gmail.com>
2020-11-07 12:20:08 -0700

    Add forecast namespace to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get '/forecast', to: 'forecast#show'
+      namespace :forecast do
+        get '/', to: 'search#show'
+      end
     end
   end
 end
