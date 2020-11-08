2ba1459196557e701e4ac5a1aa6b8c35fee13b8f
Dani Coleman <danicoleman00@gmail.com>
2020-11-08 15:27:21 -0700

    Add string interpolation to get mph for wind speed; add s to condition

--- app/poros/hourly_weather.rb
+++ app/poros/hourly_weather.rb
@@ -6,10 +6,29 @@ class HourlyWeather
               :icon
 
   def initialize(data)
-    @time = Time.now(data[:dt])
-    @wind_speed = data[:wind_speed]
+    @time = Time.at(data[:dt])
+    @wind_speed = "#{data[:wind_speed].to_s}mph"
     @wind_direction = data[:wind_deg] #find out how to do this one
-    @condition = data[:weather][0][:description]
+    @conditions = data[:weather][0][:description]
     @icon = data[:weather][0][:icon]
   end
+
+  # def wind_direction(degree)
+  #   # N 348.75 - 11.25
+  #   # NNE 11.25 - 33.75
+  #   # NE 33.75 - 56.25
+  #   # ENE 56.25 - 78.75
+  #   # E 78.75 - 101.25
+  #   # ESE 101.25 - 123.75
+  #   # SE 123.75 - 146.25
+  #   # SSE 146.25 - 168.75
+  #   # S 168.75 - 191.25
+  #   # SSW 191.25 - 213.75
+  #   # SW 213.75 - 236.25
+  #   # WSW 236.25 - 258.75
+  #   # W 258.75 - 281.25
+  #   # WNW 281.25 - 303.75
+  #   # NW 303.75 - 326.25
+  #   # NNW 326.25 - 348.75
+  # end
 end
