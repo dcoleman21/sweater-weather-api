f2c8c1bb233b30a386d63d7127a0e58659d3d7fc
Dani Coleman <danicoleman00@gmail.com>
2020-11-08 12:04:44 -0700

    Remove extra space

--- app/services/geocode_service.rb
+++ app/services/geocode_service.rb
@@ -4,5 +4,4 @@ class GeocodeService
     response = Faraday.get("#{ENV['MAP_URL']}/geocoding/v1/address?key=#{ENV['MAPQUEST_API_KEY']}&location=#{location}")
     JSON.parse(response.body, symbolize_names: true)
   end
-
 end
