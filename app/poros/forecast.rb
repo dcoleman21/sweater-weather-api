1e18912b73a1fc5c349df0ddd36023b81d4089e7
Dani Coleman <danicoleman00@gmail.com>
2020-11-08 12:06:30 -0700

    Remove commented out code

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -13,28 +13,15 @@ class Forecast
     Weather.new(weather)
   end
 
-  def daily(weather)
-    weather.map do |weather|
-      #this is coming back as [:lat, 39.74]
-      { date: Time.now(day[:dt]),
-        sunrise: Time.at(day[:sunrise]),
-        sunset: Time.at(day[:sunset]),
-        max_temp: day[:temp][:max],
-        min_temp: day[:temp][:min],
-        conditions: day[:weather][0][:description],
-        icon: day[:weather][0][:icon]}
+  def daily(weather, limit = 5)
+    weather[:daily].first.map do |data|
+      DailyWeather.new(data)
     end
   end
 
-  # def daily(weather, limit = 5)
-  #   weather[:daily][0].map do |data|
-  #     DailyWeather.new(data)
-  #   end
-  # end
-  #
-  # def hourly(weather, limit = 8)
-  #   weather[:hourly].first(limit).map do |data|
-  #     HourlyWeather.new(data)
-  #   end
-  # end
+  def hourly(weather, limit = 8)
+    weather[:hourly].first(limit).map do |data|
+      HourlyWeather.new(data)
+    end
+  end
 end
