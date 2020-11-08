ec0c6265d6fdb0773d577cd78a459a889bebfbf4
Dani Coleman <danicoleman00@gmail.com>
2020-11-07 20:03:37 -0700

    Remove namespace for forecast. Forecast now lives under the v1 namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      namespace :forecast do
-        get '/', to: 'search#show'
-      end
+      get '/forecast', to: 'weather#show'
     end
   end
 end
