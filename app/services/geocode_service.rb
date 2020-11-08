02ec36b36974e3d7ecb64146503798eede61e231
Dani Coleman <danicoleman00@gmail.com>
2020-11-08 09:04:06 -0700

    Add url env in the geocode service request

--- app/services/geocode_service.rb
+++ app/services/geocode_service.rb
@@ -1,7 +1,7 @@
 class GeocodeService
 
   def self.get_address(location)
-    response = Faraday.get("http://www.mapquestapi.com/geocoding/v1/address?key=#{ENV['MAPQUEST_API_KEY']}&location=#{location}")
+    response = Faraday.get("#{ENV['MAP_URL']}/geocoding/v1/address?key=#{ENV['MAPQUEST_API_KEY']}&location=#{location}")
     JSON.parse(response.body, symbolize_names: true)
   end
 
