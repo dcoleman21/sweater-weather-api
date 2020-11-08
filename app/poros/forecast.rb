a89473ef3ff3f28a911f14287a7228535af83419
Dani Coleman <danicoleman00@gmail.com>
2020-11-07 20:10:11 -0700

    Create Forecast poro that initialized all of the weather; current, daily, hourly

---
+++ app/poros/forecast.rb
@@ -0,0 +1,27 @@
+class Forecast
+  attr_reader :current_weather,
+              :daily_weather,
+              :hourly_weather,
+
+  def initialize(data)
+    @current_weather = current(data[:current])
+    @daily_weather = daily(data[:daily], 5)
+    @hourly_weather = hourly(data[:hourly], 8)
+  end
+
+  def current(weather)
+    Weather.new(weather)
+  end
+#these are not created yet
+  # def daily(weather, limit = 5)
+  #   weather[:daily].first(limit).map do |data|
+  #     DailyWeather.new(data)
+  #   end
+  # end
+  #
+  # def hourly(weather, limit = 8)
+  #   weather[:hourly].first(limit).map do |data|
+  #     HourlyWeather.new(data)
+  #   end
+  # end
+end
