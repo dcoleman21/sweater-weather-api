4192fda0cc732915a6c758b031340863ced05efa
Dani Coleman <danicoleman00@gmail.com>
2020-11-08 12:07:54 -0700

    Change method name to match how I am calling it in my facade

--- app/controllers/api/v1/weather_controller.rb
+++ app/controllers/api/v1/weather_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::WeatherController < ApplicationController
   def show
     location = params[:location]
-    coords = GeoFacade.fetch_coordinates(location)
+    coords = GeoFacade.location(location)
     forecast = WeatherFacade.fetch_weather(coords.lat, coords.lon)
     render json: ForecastSerializer.new(forecast)
   end
