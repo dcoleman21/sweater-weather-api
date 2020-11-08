7032585c5a653b9619013a7fed6c54f95aeaa863
Dani Coleman <danicoleman00@gmail.com>
2020-11-08 00:43:24 -0700

    WIP trying to figure out why things are not working

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -3,18 +3,31 @@ class Forecast
               :daily_weather,
               :hourly_weather
 
-  def initialize(data)
-    @current_weather = current(data[:current])
-    # @daily_weather = daily(data[:daily], 5)
-    # @hourly_weather = hourly(data[:hourly], 8)
+  def initialize(weather)
+    @current_weather = current(weather)
+    @daily_weather   = daily(weather)
+    @hourly_weather  = hourly(weather)
   end
 
   def current(weather)
     Weather.new(weather)
   end
-#these are not created yet
+
+  def daily(weather)
+    weather.map do |weather|
+      #this is coming back as [:lat, 39.74]
+      { date: Time.now(day[:dt]),
+        sunrise: Time.at(day[:sunrise]),
+        sunset: Time.at(day[:sunset]),
+        max_temp: day[:temp][:max],
+        min_temp: day[:temp][:min],
+        conditions: day[:weather][0][:description],
+        icon: day[:weather][0][:icon]}
+    end
+  end
+
   # def daily(weather, limit = 5)
-  #   weather[:daily].first(limit).map do |data|
+  #   weather[:daily][0].map do |data|
   #     DailyWeather.new(data)
   #   end
   # end
