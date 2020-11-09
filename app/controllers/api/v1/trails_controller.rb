2505b8da4783716b5f49e06b9e8cc93a64079211
Dani Coleman <danicoleman00@gmail.com>
2020-11-09 15:18:48 -0700

    Call trails facade and render the trails serializer

--- app/controllers/api/v1/trails_controller.rb
+++ app/controllers/api/v1/trails_controller.rb
@@ -1,16 +1,7 @@
 class Api::V1::TrailsController < ApplicationController
-  # def index
-  #   location = params[:location]
-  #   coordinates = GeoFacade.location(location)
-  #   weather = WeatherFacade.fetch_weather(coordinates.lat, coordinates.lon)
-  #   expected_weather = weather.current_weather
-  #   trails = TrailFacade.trails_for_location(coordinates)
-  # end
 
   def index
-    location = params[:location]
-    coordinates = GeoFacade.location(location)
-    dest = Destination.new(coordinates, location)
-    render json: DestinationSerializer.new(dest)
+    trails = TrailFacade.trails_for_location(params[:location])
+    render json: TrailSerializer.new(trails)
   end
 end
