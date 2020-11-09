a9261e2d05c37cb18644f840978d8fac1da2d65d
Dani Coleman <danicoleman00@gmail.com>
2020-11-09 10:50:08 -0700

    Add facade calls from geo, weather and trail

--- app/controllers/api/v1/trails_controller.rb
+++ app/controllers/api/v1/trails_controller.rb
@@ -1,4 +1,9 @@
-class API::V1::TrailsController < ApplicationController
+class Api::V1::TrailsController < ApplicationController
   def index
+    coordinates = GeoFacade.location(params[:location])
+    weather = WeatherFacade.fetch_weather(coordinates.lat, coordinates.lon)
+    expected_weather = weather.current_weather
+    trails = TrailFacade.trails_for_location(coordinates)
+    # require "pry"; binding.pry
   end
 end
