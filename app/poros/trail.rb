72340c3219f8a8f38bfa98f6cf04896b3c01fcbf
Dani Coleman <danicoleman00@gmail.com>
2020-11-09 12:51:27 -0700

    Add method to get distance to trail

--- app/poros/trail.rb
+++ app/poros/trail.rb
@@ -10,12 +10,12 @@ class Trail
     @summary = data[:summary]
     @difficulty = data[:difficulty]
     @location = data[:location]
-    # @start_point = coord
-    # @distance_to_trail = distance_to_trail
+    @start_point = coord
+    @distance_to_trail = distance_to_trail
   end
 
-  # def distance_to_trail
-  #   x = GeoFacade.get_distance(@location, @start_point)
-  #   x[:route][:distance]
-  # end
+  def distance_to_trail
+    x = GeoFacade.get_distance(@location, @start_point)
+    x[:route][:distance]
+  end
 end
