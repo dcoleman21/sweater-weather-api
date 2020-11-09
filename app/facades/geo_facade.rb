d192e092610471dc4a6ceb818ba1bf05d950cbcd
Dani Coleman <danicoleman00@gmail.com>
2020-11-09 12:54:21 -0700

    Change Geocode to GeoData

--- app/facades/geo_facade.rb
+++ app/facades/geo_facade.rb
@@ -1,11 +1,11 @@
 class GeoFacade
 
   def self.location(location)
-    response = GeocodeService.get_address(location)
+    response = GeoDataService.get_address(location)
     Location.new(response)
   end
 
   def self.get_distance(destination, start_point)
-    response = GeocodeService.get_distance(destination, start_point)
+    response = GeoDataService.get_distance(destination, start_point)
   end
 end
