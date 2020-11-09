ef4b2e2e101d0211913fcf59f6229db414057860
Dani Coleman <danicoleman00@gmail.com>
2020-11-09 12:48:43 -0700

    WIP adding expectations

--- spec/requests/api/v1/trail_request_spec.rb
+++ spec/requests/api/v1/trail_request_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'Trail request' do
   it "returns json data of trail data for location" do
+    # location = 'denver,co'
+    # latitude = 39.738453
+    # longitude = -104.984853
+
     get '/api/v1/trails?location=denver,co'
 
     expect(response).to be_successful
-    # trails = JSON.pars(response.body, symbolize_names: true)
+    trails = JSON.parse(response.body, symbolize_names: true)
   end
 end
