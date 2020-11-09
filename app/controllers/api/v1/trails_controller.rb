99bbfc5f71b5b75ad740be5852f8241232db7ce9
Dani Coleman <danicoleman00@gmail.com>
2020-11-09 12:54:57 -0700

    Add calls to index action for trails controller

--- app/controllers/api/v1/trails_controller.rb
+++ app/controllers/api/v1/trails_controller.rb
@@ -1,9 +1,16 @@
 class Api::V1::TrailsController < ApplicationController
+  # def index
+  #   location = params[:location]
+  #   coordinates = GeoFacade.location(location)
+  #   weather = WeatherFacade.fetch_weather(coordinates.lat, coordinates.lon)
+  #   expected_weather = weather.current_weather
+  #   trails = TrailFacade.trails_for_location(coordinates)
+  # end
+
   def index
     location = params[:location]
     coordinates = GeoFacade.location(location)
-    weather = WeatherFacade.fetch_weather(coordinates.lat, coordinates.lon)
-    expected_weather = weather.current_weather
-    trails = TrailFacade.trails_for_location(coordinates)
+    dest = Destination.new(coordinates, location)
+    render json: DestinationSerializer.new(dest)
   end
 end
