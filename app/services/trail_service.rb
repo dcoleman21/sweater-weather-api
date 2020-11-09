9a07fca717fe2b8353e74ff99b47f04b39aaca81
Dani Coleman <danicoleman00@gmail.com>
2020-11-09 15:15:39 -0700

    Add two arguments lat and lon

--- app/services/trail_service.rb
+++ app/services/trail_service.rb
@@ -1,7 +1,6 @@
 class TrailService
-  def self.get_trails(data)
-    lat = data.lat
-    lon = data.lon
+
+  def self.get_trails(lat, lon)
     response = Faraday.get("#{ENV['TRAIL_URL']}/data/get-trails?lat=#{lat}&lon=#{lon}&maxDistance=10&key=#{ENV['TRAIL_API_KEY']}")
     JSON.parse(response.body, symbolize_names: true)
   end
