444d784ad5846c613e997f8804a1dc1ea4fd6618
Dani Coleman <danicoleman00@gmail.com>
2020-11-09 15:18:06 -0700

    Add facades from map and weather to get coords and forecast; Add trail service and trail poro

--- app/facades/trail_facade.rb
+++ app/facades/trail_facade.rb
@@ -1,6 +1,8 @@
 class TrailFacade
-  def self.trails_for_location(data)
-    result = TrailService.get_trails(data)
-    Trail.new(result, data)
+  def self.trails_for_location(location)#"denver,co"
+    coordinates = GeoFacade.location(location)#<Location:0x00007f8c155a04d0 @lat=39.738453, @lon=-104.984853>
+    forecast = WeatherFacade.fetch_weather(coordinates.lat, coordinates.lon)#entire forecast object(current, daily, hourly) for denver
+    result = TrailService.get_trails(coordinates.lat, coordinates.lon)#entire trails array for denver
+    Trail.new(result, forecast, coordinates, location)
   end
 end
