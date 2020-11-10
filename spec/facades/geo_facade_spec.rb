80dc3ec08438468da4716356f93152f4d121b923
Dani Coleman <danicoleman00@gmail.com>
2020-11-09 22:23:48 -0700

    Add test for getting a location distance

--- spec/facades/geo_facade_spec.rb
+++ spec/facades/geo_facade_spec.rb
@@ -6,7 +6,18 @@ RSpec.describe GeoFacade do
     geocode = GeoFacade.location(location)
 
     expect(geocode).to be_a(Location)
-    expect(geocode.lat).to be_a(Float)
-    expect(geocode.lon).to be_a(Float)
+    expect(geocode.lat).to be_a(Numeric)
+    expect(geocode.lon).to be_a(Numeric)
+  end
+
+  it "can location distance" do
+    lat1 = 39.738453
+    lon1 = -104.982323
+    lat2 = 39.95816
+    lon2 = -105.257965
+
+    distance = GeoFacade.get_distance(lat1, lon1, lat2, lon2)
+
+    expect(distance).to be_a(Numeric)
   end
 end
