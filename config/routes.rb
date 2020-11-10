b0bc625d59ee6437047c17868a42a466ef812f8e
Dani Coleman <danicoleman00@gmail.com>
2020-11-10 09:55:08 -0700

    Add munchies to routes; index action

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/forecast', to: 'weather#show'
       get '/trails', to: 'trails#index'
+      get '/munchies', to: 'munchies#index'
     end
   end
 end
