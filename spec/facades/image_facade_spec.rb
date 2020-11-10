67e95621d88581fa644641514949d410cd7e265d
Dani Coleman <danicoleman00@gmail.com>
2020-11-09 20:50:09 -0700

    Add test for image facade

--- spec/facades/image_facade_spec.rb
+++ spec/facades/image_facade_spec.rb
@@ -3,5 +3,11 @@ require 'rails_helper'
 RSpec.describe ImageFacade do
   it "gets an image object" do
     location = 'denver,co'
+
+    photo = ImageFacade.location_background(location)
+
+    expect(photo).to be_a(Image)
+    expect(photo.image_url).to be_a(String)
+    expect(photo.credit).to be_a(String)
   end
 end
