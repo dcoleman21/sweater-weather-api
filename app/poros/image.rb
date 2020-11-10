a78854d2c50d90b28c511e526fe5ffa1c12e467a
Dani Coleman <danicoleman00@gmail.com>
2020-11-09 22:12:05 -0700

    Add initialize method and method to make credit a hash with key/value

--- app/poros/image.rb
+++ app/poros/image.rb
@@ -1,3 +1,15 @@
 class Image
-  
+  attr_reader :image_url, :credit
+
+  def initialize(data)
+    @image_url = data[:photos][1][:src][:original]
+    @credit = credit_hash(data)
+  end
+
+  def credit_hash(data)
+    {
+      source: data[:photos][1][:photographer_url],
+      author: data[:photos][1][:photographer]
+    }
+  end
 end
