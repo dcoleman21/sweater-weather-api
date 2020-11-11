843c8bc39cbf9461591b5244e8bbe90f22f10570
Dani Coleman <danicoleman00@gmail.com>
2020-11-11 14:07:10 -0700

    Remove uneeded attributes and global variables from the intialize method; Call the image hash method in the initialize method and call the credit hash within the image hash

--- app/poros/image.rb
+++ app/poros/image.rb
@@ -1,15 +1,22 @@
 class Image
-  attr_reader :image_url, :credit
+  attr_reader :image
 
-  def initialize(data)
-    @image_url = data[:photos][1][:src][:original]
-    @credit = credit_hash(data)
+  def initialize(data, location)
+    @image = image_hash(data, location)
+  end
+
+  def image_hash(data, location)
+    {
+      location: location,
+      image_url: data[:photos][0][:src][:original],
+      credit: credit_hash(data)
+    }
   end
 
   def credit_hash(data)
     {
-      source: data[:photos][1][:photographer_url],
-      author: data[:photos][1][:photographer]
+      source: data[:photos][0][:photographer_url],
+      author: data[:photos][0][:photographer]
     }
   end
 end
