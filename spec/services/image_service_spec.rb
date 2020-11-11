a2e8487891cbc3d1bc66cb2158f0dd2fafc7e5ab
Dani Coleman <danicoleman00@gmail.com>
2020-11-11 13:59:45 -0700

    Change test to test the first response returned rather than the second.

--- spec/services/image_service_spec.rb
+++ spec/services/image_service_spec.rb
@@ -2,19 +2,19 @@ require 'rails_helper'
 RSpec.describe ImageService do
   it "returns image data for location in json" do
     location = 'denver,co'
-    
+
     result = ImageService.location_background(location)
-    # require "pry"; binding.pry
+    
     expect(result).to be_a(Hash)
     expect(result).to have_key(:photos)
     expect(result[:photos]).to be_an(Array)
-    expect(result[:photos][1]).to have_key(:url)
-    expect(result[:photos][1][:url]).to be_a(String)
-    # expect(result[:photos][1]).to have_key(:photographer)
-    # expect(result[:photos][1][:photographer]).to be_a(String)
-    # expect(result[:photos][1]).to have_key(:src)
-    # expect(result[:photos][1][:src]).to be_a(Hash)
-
+    expect(result[:photos][0]).to have_key(:url)
+    expect(result[:photos][0][:url]).to be_a(String)
+    expect(result[:photos][0]).to have_key(:photographer)
+    expect(result[:photos][0][:photographer]).to be_a(String)
+    expect(result[:photos][0]).to have_key(:src)
+    expect(result[:photos][0][:src]).to be_a(Hash)
+    expect(result[:photos][0][:src]).to have_key(:original)
+    expect(result[:photos][0][:src][:original]).to be_a(String)
   end
-
 end
