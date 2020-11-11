0886ff7eabf3af9c5d9c6edb224f22c5c22d738b
Dani Coleman <danicoleman00@gmail.com>
2020-11-11 14:02:56 -0700

    Add more expects for the serialized image hash

--- spec/facades/image_facade_spec.rb
+++ spec/facades/image_facade_spec.rb
@@ -6,10 +6,17 @@ RSpec.describe ImageFacade do
 
     photo = ImageFacade.location_background(location)
 
-    expect(photo).to be_a(Image)
-    expect(photo.image_url).to be_a(String)
-    expect(photo.credit).to be_a(Hash)
-    expect(photo.credit[:source]).to be_a(String)
-    expect(photo.credit[:author]).to be_a(String)
+    expect(photo).to be_an(Image)
+    expect(photo.image).to be_a(Hash)
+    expect(photo.image).to have_key(:location)
+    expect(photo.image[:location]).to be_a(String)
+    expect(photo.image).to have_key(:image_url)
+    expect(photo.image[:image_url]).to be_a(String)
+    expect(photo.image).to have_key(:credit)
+    expect(photo.image[:credit]).to be_a(Hash)
+    expect(photo.image[:credit]).to have_key(:source)
+    expect(photo.image[:credit][:source]).to be_a(String)
+    expect(photo.image[:credit]).to have_key(:author)
+    expect(photo.image[:credit][:author]).to be_a(String)
   end
 end
