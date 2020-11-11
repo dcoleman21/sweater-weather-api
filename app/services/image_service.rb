ffac8ab735e817901a306575a9063ccad122cc7d
Dani Coleman <danicoleman00@gmail.com>
2020-11-11 14:04:12 -0700

    Update query params to include more helper phrases and to return only the first page when called

--- app/services/image_service.rb
+++ app/services/image_service.rb
@@ -1,8 +1,8 @@
 class ImageService
   def self.location_background(location)
-    response = Faraday.get("#{ENV['IMAGE_URL']}/v1/search?query=denver,co&page=5") do |req|
-      req.params['query'] = location
-      req.params['page'] = 5
+    response = Faraday.get("#{ENV['IMAGE_URL']}/v1/search") do |req|
+      req.params['query'] = "#{location} skyline"
+      req.params['page'] = 1
       req.headers['Authorization'] = ENV['IMAGE_API_KEY']
     end
     JSON.parse(response.body, symbolize_names: true)  end
